18bb668c7ed9d310188c7467eab86593be02fb0d
eankim <eank0108@gmail.com>
2017-01-15 10:56:01 +0900

    2nd commit

--- app/src/main/java/com/composing/ean/composing/MainActivity.java
+++ app/src/main/java/com/composing/ean/composing/MainActivity.java
@@ -2,12 +2,15 @@ package com.composing.ean.composing;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tv = (TextView) findViewById(R.id.text);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
